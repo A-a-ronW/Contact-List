175267a460f751f0cf0941b77a380918ee7bc842
Aaron <aaron012301@gmail.com>
2022-07-29 14:23:31 -0400

    Make 'Add' insert readonly inputs instead of spans

--- script.js
+++ script.js
@@ -19,21 +19,25 @@ window.addEventListener('load', () => {
         const contact_element = document.createElement("div");
         contact_element.classList.add("contact");
 
-        const contactName = document.createElement("span");
+        const contactName = document.createElement("input");
         contactName.classList.add("contact-name");
-        contactName.innerText = name;
+        contactName.type=("text");
+        contactName.value = name;
+        contactName.setAttribute("readonly", "readonly");
 
-        const contactNum = document.createElement("span");
+        const contactNum = document.createElement("input");
         contactNum.classList.add("contact-num");
-        contactNum.innerText = number;
+        contactNum.type=("text");
+        contactNum.value = number;
+        contactNum.setAttribute("readonly", "readonly");
 
         const editBtn = document.createElement("button");
         editBtn.classList.add("editBtn");
-        editBtn.innerText = "Edit";
+        editBtn.innerHTML = "Edit";
 
         const deleteBtn = document.createElement("button");
         deleteBtn.classList.add("deleteBtn");
-        deleteBtn.innerText = "Delete";
+        deleteBtn.innerHTML = "Delete";
 
         contact_element.appendChild(contactName);
         contact_element.appendChild(contactNum);
