ac21e83cd8e395b245a0052b2b04b35adcd70688
Aaron <aaron012301@gmail.com>
2022-07-28 22:44:57 -0400

    Change contact input to span

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.addEventListener('load', () => {
     const form = document.querySelector("#contact-input");
     const inputName = document.querySelector("#name-entry");
     const inputNum = document.querySelector("#number-entry");
-    const list = document.querySelector("contacts");
+    const list = document.querySelector("#contacts");
 
     form.addEventListener('submit', (e) => {
         e.preventDefault();
